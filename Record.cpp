dbc1d180dbd6e5d3f8144db0ee008b84f0b1d21d
john-fotis <giannisfotis@gmai.com>
2021-03-12 06:29:21 +0200

    Definition and implementation of Record object

--- Record.cpp
+++ Record.cpp
@@ -1,30 +1,20 @@
 #include "Record.hpp"
 
+Record::Record(const Record &record) {
+  if (this == &record) return;
+  citizenID = record.ID();
+  date = record.getDate();
+}
+
 Record &Record::operator=(const Record &record) {
   if (this == &record) return *this;
-  citizenID = record.citizenID;
-  firstName = record.firstName;
-  lastName = record.lastName;
-  country = record.country;
-  age = record.age;
-  virusName = record.virusName;
-  status = record.status;
-  date = record.date;
+  citizenID = record.ID();
+  date = record.getDate();
   return *this;
 }
 
 bool operator==(const Record &r1, const Record &r2) {
-  return (
-      r1.citizenID == r2.citizenID &&
-      r1.firstName == r2.firstName &&
-      r1.lastName == r2.lastName &&
-      r1.country == r2.country &&
-      r1.age == r2.age &&
-      r1.virusName == r2.virusName &&
-      r1.status == r2.status &&
-      r1.citizenID == r2.citizenID &&
-      r1.date == r2.date
-  );
+  return (r1.ID() == r2.ID() && r1.getDate() == r2.getDate());
 }
 
 bool operator!=(const Record &r1, const Record &r2) {
@@ -32,43 +22,36 @@ bool operator!=(const Record &r1, const Record &r2) {
 }
 
 bool operator<(const Record &r1, const Record &r2) {
-  return r1.citizenID < r2.citizenID;
+  return r1.ID() < r2.ID();
 }
+
 bool operator>(const Record &r1, const Record &r2) {
-  return r1.citizenID > r2.citizenID;
+  return r1.ID() > r2.ID();
 }
+
 bool operator<=(const Record &r1, const Record &r2) {
-  return r1.citizenID <= r2.citizenID;
+  return r1.ID() <= r2.ID();
 }
+
 bool operator>=(const Record &r1, const Record &r2) {
-  return r1.citizenID >= r2.citizenID;
+  return r1.ID() >= r2.ID();
 }
 
 std::ostream &operator<<(std::ostream &os, const Record &record) {
-  os << record.citizenID << " " << record.firstName << " "
-     << record.lastName << " " << record.country << " "
-     << record.age << " " << record.virusName << " " << record.status;
-  std::string str(record.status);
-  if (!str.compare("YES")) os << " " << record.date;
+  os << record.ID() << " " << record.getDate();
   return os;
 }
 
-void Record::set(unsigned int id, std::string name,
-                std::string surname, std::string country,
-                unsigned int age, std::string virus,
-                std::string status, Date date) {
+void Record::set(unsigned int id, Date date) {
   this->citizenID = id;
-  this->firstName = name;
-  this->lastName = surname;
-  this->country = country;
-  this->age = age;
-  this->virusName = virus;
-  this->status = status;
   this->date = date;
 }
 
+void Record::set(unsigned int id, std::string dateStr) {
+  this->citizenID = id;
+  this->date.set(dateStr);
+}
+
 void Record::print() const {
-  std::cout << citizenID << " " << firstName << " " << lastName << " "
-  << country << " " << age << " " << virusName << " " << status << " ";
-  date.print();
+  std::cout << citizenID << " " << date;
 }
--- Record.hpp
+++ Record.hpp
@@ -2,29 +2,20 @@
 #define RECORD_HPP
 
 #include <iostream>
+
 #include "Date.hpp"
 
 class Record {
 private:
   unsigned int citizenID;
-  std::string firstName;
-  std::string lastName;
-  std::string country;
-  unsigned int age;
-  std::string virusName;
-  std::string status; // [YES/NO]
   Date date;
+  
 public:
-  Record() { citizenID = 0; age = 0; }
+  Record(int id = 0) : citizenID(id) {}
   ~Record() {}
+  Record(const Record &record);
 
   unsigned int ID() const { return citizenID; }
-  std::string getFirstName() const { return firstName; }
-  std::string getLastName() const { return lastName; }
-  std::string getCountry() const { return country; }
-  unsigned int getAge() const { return age; }
-  std::string getVirusName() const { return virusName; }
-  std::string getStatus() const { return status; }
   std::string getDate() const { std::string str(""); date.get(str); return str; }
 
   Record &operator=(const Record &record);
@@ -36,10 +27,8 @@ public:
   friend bool operator>=(const Record &r1, const Record &r2);
   friend std::ostream &operator<<(std::ostream &os, const Record &record);
 
-  void set(unsigned int id, std::string name,
-          std::string surname, std::string country,
-          unsigned int age, std::string virus,
-          std::string status, Date date);
+  void set(unsigned int id, Date date);
+  void set(unsigned int id, std::string dateStr);
   void print() const;
 };
 
