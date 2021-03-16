450a199ca682840e1315a05f45ff7fa14f7fa936
john-fotis <giannisfotis@gmai.com>
2021-03-16 05:25:55 +0200

    Changed hash function to work with std::string instead of unsigned int and implemented the Virus-Country Entry type

--- HashTable.cpp
+++ HashTable.cpp
@@ -2,23 +2,24 @@
 
 #include "HashTable.hpp"
 #include "Person.hpp"
+#include "VirusCountryEntry.hpp"
 
 template <typename T>
-void HashTable<T>::insert(unsigned int key, T node) {
+void HashTable<T>::insert(std::string key, T node) {
   unsigned long index = hashFunction(key);
   table[index].insertFirst(node);
   totalEntries++;
 }
 
 template <typename T>
-void HashTable<T>::remove(unsigned int key, T node) {
+void HashTable<T>::remove(std::string key, T node) {
   unsigned long index = hashFunction(key);
   table[index].popValue(node);
   totalEntries--;
 }
 
 template <typename T>
-T *HashTable<T>::search(unsigned int key, T node) {
+T *HashTable<T>::search(std::string key, T node) {
   unsigned long index = hashFunction(key);
   T *temp = NULL;
   temp = table[index].search(node);
@@ -40,4 +41,5 @@ void HashTable<T>::print() const {
 template class HashTable<int>;
 template class HashTable<std::string>;
 template class HashTable<Person>;
+template class HashTable<VirusCountryEntry>;
 // ... and so on ...
--- HashTable.hpp
+++ HashTable.hpp
@@ -2,6 +2,7 @@
 #define HASHTABLE_H
 
 #include "List.hpp"
+#include "hashFunctions.hpp"
 
 #define TABLE_CAPACITY 1000
 
@@ -12,12 +13,8 @@ private:
   unsigned int totalEntries;
   List<T> *table; // Array of collision buckets used for the chains
 
-  unsigned long hashFunction(unsigned int key) {
-    unsigned long hash = 0;
-    hash = ((key >> 16) ^ key) * 0x45d9f3b;
-    hash = ((hash >> 16) ^ hash) * 0x45d9f3b;
-    hash = ((hash >> 16) ^ hash);
-    return hash % size;
+  unsigned long hashFunction(std::string key) {
+    return hash_i((unsigned char *)key.c_str(), key.length()) % size;
   }
 public:
   HashTable(unsigned int capacity=TABLE_CAPACITY)
@@ -27,10 +24,10 @@ public:
   unsigned int getSize() const { return size; }
   unsigned int getTotalEntries() const { return totalEntries; }
 
-  void insert(unsigned int key, T node);
-  void remove(unsigned int key, T node);
+  void insert(std::string key, T node);
+  void remove(std::string key, T node);
 
-  T *search(unsigned int key, T node);
+  T *search(std::string key, T node);
   void print() const;
 };
 
