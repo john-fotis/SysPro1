5eb817d359a3973d7528ba7cf75194d4ad9346e4
john-fotis <giannisfotis@gmai.com>
2021-03-16 05:23:26 +0200

    Minor Change

--- Country.hpp
+++ Country.hpp
@@ -7,11 +7,12 @@ class Country {
 private:
   std::string name;
   unsigned int population;
-  
+
 public:
-  Country() : name(""), population(1) {}
+  Country() : name(""), population(0){}
   ~Country() {}
   Country(const Country &country);
+  Country &operator=(const Country &country);
 
   std::string getName() const { return name; }
   unsigned int getPopulation() const { return population; }
@@ -19,7 +20,6 @@ public:
   void setName(const std::string &str) { name.clear(); name.assign(str); }
   void addPerson() { population++; }
 
-  Country &operator=(const Country &country);
   friend bool operator==(const Country &c1, const Country &c2);
   friend bool operator!=(const Country &c1, const Country &c2);
   friend bool operator<(const Country &c1, const Country &c2);
