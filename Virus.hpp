ab707244a28bd4ce4c8cf234d780081bc30579e1
john-fotis <giannisfotis@gmai.com>
2021-03-16 05:32:19 +0200

    Added constructor for bloomSize and more class functions to handle the skip-lists and bloom-filter

--- Virus.hpp
+++ Virus.hpp
@@ -13,15 +13,37 @@ private:
   BloomFilter filter;
   SkipList<Record> vaccinatedList;
   SkipList<int> nonVaccinatedList;
+
 public:
   Virus() : name("") {}
+  Virus(unsigned int bloomSize) : name(""), filter(bloomSize) {}
   ~Virus() {}
   Virus (const Virus &virus);
+  Virus &operator=(const Virus &virus);
 
   std::string getName() const { return name; }
-  void setName(const std::string &str) { name = str; }
+  unsigned int getBloomSize() const { return filter.getSize(); }
+  unsigned int getVaccinatedListSize() const { return vaccinatedList.getSize(); }
+  unsigned int getNonVaccinatedListSize() const { return nonVaccinatedList.getSize(); }
 
-  Virus &operator=(const Virus &virus);
+  void setName(const std::string &str) { name = str; }
+  void initializeBloom(const Virus &virus) { filter = virus.filter; }
+
+  void insertBloom(const std::string &str) { filter.insert(str); }
+  bool checkBloom(const std::string &str) { return filter.check(str); }
+  void bloomStatus() const { filter.arrayStatus(); }
+
+  void insertVaccinatedList(const Record &record) { vaccinatedList.insert(record); }
+  void insertNonVaccinatedList(const int id) {nonVaccinatedList.insert(id); }
+  void removeVaccinatedList(const Record &record) { vaccinatedList.remove(record); }
+  void removeNonVaccinatedList(const int id) { nonVaccinatedList.remove(id); }
+
+  Record *searchVaccinatedList(const Record record) { return vaccinatedList.search(record); }
+  int *searchNonVaccinatedList(const int id) { return nonVaccinatedList.search(id); }
+  // Used to read each record of the skiplist at given position
+  Record *getPositiveRecordNumber(unsigned int num) { return vaccinatedList.getNode(num); }
+  int *getNegativeRecordNumber(unsigned int num) { return nonVaccinatedList.getNode(num); }
+
   friend bool operator==(const Virus &v1, const Virus &v2);
   friend bool operator!=(const Virus &v1, const Virus &v2);
   friend bool operator<(const Virus &v1, const Virus &v2);
@@ -29,10 +51,6 @@ public:
   friend bool operator<=(const Virus &v1, const Virus &v2);
   friend bool operator>=(const Virus &v1, const Virus &v2);
   friend std::ostream &operator<<(std::ostream &os, const Virus &virus);
-
-  void insertBloom(const std::string &str) { filter.insert(str); }
-  bool checkBloom(const std::string &str) { return filter.check(str); }
-  void bloomStatus() const { filter.arrayStatus(); }
 };
 
 #endif
